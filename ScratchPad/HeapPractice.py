cc3ce1c37c65b45836d2044b6a9b72cb32c2e06b
manrabs <muhammedrabiuumar@gmail.com>
2025-01-27 13:59:01 -0600

    added some optimzations for better bubble/selection sort solution. also added some more variations for insertion sort practice in heappractice.py file

--- ScratchPad/HeapPractice.py
+++ ScratchPad/HeapPractice.py
@@ -1,8 +1,69 @@
 import heapq 
-A = [-4, 3, 1, 0, 2, 5, 10, 8, 12, 9]
-n = len(A)
+# A = [-4, 3, 1, 0, 2, 5, 10, 8, 12, 9]
+# n = len(A)
 
-for i in range(n):
-    A[i] *= -1
-heapq.heapify(A)
-print(A)
+# for i in range(n):
+#     A[i] *= -1
+# heapq.heapify(A)
+# print(A)
+
+class Nodes:
+    def find_missing_children(heap):
+        n = len(heap)
+        missing_nodes = []
+
+        for i in range(n):
+            left_child = 2 * i + 1
+            right_child = 2 * i + 2
+
+            missing = []
+            if left_child >= n:
+                missing.append("left")
+            if right_child >= n:
+                missing.append("right")
+
+            if missing:
+                missing_nodes.append({
+                    "node": heap[i],
+                    "missing_children": missing
+                })
+
+        return missing_nodes
+    
+    def insert_last(heap, val):
+        # heapq.heappush(heap, val)
+        heap.append(val)
+        n = len(heap)
+        i = n - 1
+        while i > 0 and heap[i] < heap[(i-1)//2]:
+            heap[i], heap[(i-1)//2] = heap[(i-1)//2], heap[i]
+            i = (i-1)//2
+    
+    def insert_first(heap, val):
+        heap.insert(0, val)
+        n = len(heap)
+        i = 0
+
+        while True:
+            smallest = i
+            left_child = 2 * i + 1
+            right_child = 2 * i + 2
+
+            if left_child < n and heap[left_child] < heap[smallest]:
+                smallest = left_child
+            if right_child < n and heap[right_child] < heap[smallest]:
+                smallest = right_child
+            if smallest != i:
+                heap[i], heap[smallest] = heap[smallest], heap[i]
+                i = smallest
+            else:
+                break
+# # Example usage
+if __name__ == "__main__":
+    heap = [1, 3, 6, 5, 9, 8]
+    result = Nodes.find_missing_children(heap)
+    print(result)
+    result2 = Nodes.insert_last(heap, 4)
+    print(heap)
+    Nodes.insert_first(heap, 7)
+    print(heap)
--- ScratchPad/selectionsort.py
+++ ScratchPad/selectionsort.py
@@ -1,12 +1,24 @@
 class BubbleSort:
     def selection(arr):
         n = len(arr)
+        # textbook approach
         for i in range(n):
             min_index = i
             for j in range(i+1, n):
                 if arr[j] < arr[min_index]:
-                    arr[j], arr[min_index] = arr[min_index], arr[j]
+                    min_index = j
+            if min_index != i:
+                arr[i], arr[min_index] = arr[min_index], arr[i]
         return arr
+
+        # my intuitive approach, works but not efficient as it has more swaps.
+        # for i in range(n):
+        #     min_index = i
+        #     for j in range(i+1, n):
+        #         if arr[j] < arr[min_index]:
+        #             min_index = j
+        #             arr[j], arr[min_index] = arr[min_index], arr[j]
+        # return arr
 if __name__ == "__main__":
-    arr = [50,2,-3,4,-2,2,5,1,-1,4,10]
+    arr = [50,2,-3,-1, 10]
     print(BubbleSort.selection(arr))
